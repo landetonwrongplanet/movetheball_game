07d7841d488970973d6981bfd3df24a5ba293e80
landetonwrongplanet <92033686+landetonwrongplanet@users.noreply.github.com>
2023-03-16 10:54:55 +0100

    Added arguments to classes as prepared in diagram

--- app/src/main/java/com/example/movetheball/GameView.java
+++ app/src/main/java/com/example/movetheball/GameView.java
@@ -1,4 +1,22 @@
 package com.example.movetheball;
 
-public class GameView {
+import android.content.Context;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class GameView extends SurfaceView implements Runnable {
+    private SurfaceHolder surfaceHolder;
+    private Paint paint;
+    private Thread gameThread;
+    private boolean isRunning;
+    private int ballX;
+    private int ballY;
+    private int ballRadius;
+    private int ballColor;
+    private int ballGlowRadius;
+    private int ballGlowColor;
+    public GameView(Context context) {
+        super(context);
+    }
 }
--- app/src/main/java/com/example/movetheball/SensorGameActivity.java
+++ app/src/main/java/com/example/movetheball/SensorGameActivity.java
@@ -1,4 +1,20 @@
 package com.example.movetheball;
 
-public class SensorGameActivity {
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SensorGameActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensormanager;
+    private Sensor accelerometer;
+    private GameView gameView;
+    private int screenWidth;
+    private int screenHeight;
+    private int cornerRadius;
+    private int cornerMargin;
+    private int cornerPadding;
+    private int cornerX;
+    private int cornerY;
+    private boolean cornerTouched;
 }
